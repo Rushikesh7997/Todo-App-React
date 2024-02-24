7e6631dced69bb1b65b5a79ae274b0ddc89ad833
Rushikesh7997 <pardhikar26@gmail.com>
2024-02-24 23:21:54 +0530

    router update

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import { EditNote } from "./pages/EditNote";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Notes></Notes>} />
-        <Route path="/create-note" element={<CreateNote></CreateNote>} />
-        <Route path="/edit-note/:id" element={<EditNote></EditNote>} />
-      </Routes>
-    </BrowserRouter>
+    <main id="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Notes></Notes>} />
+          <Route path="/create-note" element={<CreateNote></CreateNote>} />
+          <Route path="/edit-note/:id" element={<EditNote></EditNote>} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
